25b7aa39d2008c83b7069c83807ecbddf225a49a
Ticoo <JLRGeorge.Lo@gmail.com>
2023-12-03 10:35:39 +0000

    hp complete

--- Assets/scripts/healthSlider.cs
+++ Assets/scripts/healthSlider.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using TMPro;
+
 
 public class healthSlider : MonoBehaviour
 {
     public gameHealthScriptableObject gameHealthSO;
     public Image healthBar;
-    [SerializeField]
-    public Image healthTxt;
     private float amtSlider;
     // Start is called before the first frame update
     void Start()
     {
         gameHealthSO.healthChangeEvent.AddListener(changeHealthBar);
+        healthBar.GetComponentInChildren<TMP_Text>().text = string.Format("{0}/{1}", gameHealthSO.health, gameHealthSO.maxHealth);
     }
 
     void changeHealthBar()
@@ -22,9 +23,9 @@ public class healthSlider : MonoBehaviour
         if (healthBar != null)
         {
             amtSlider = ((float)gameHealthSO.health / (float)gameHealthSO.maxHealth);
-            Debug.Log(amtSlider);
+
             healthBar.GetComponent<Image>().fillAmount = amtSlider;
-            // healthTxt.GetComponent<text = string.Format("{0}/{1}", gameHealthSO.health, gameHealthSO.maxHealth);
+            healthBar.GetComponentInChildren<TMP_Text>().text = string.Format("{0}/{1}", gameHealthSO.health, gameHealthSO.maxHealth);
         }
     }
 
