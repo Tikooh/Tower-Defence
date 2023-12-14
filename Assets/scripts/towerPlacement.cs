e89ce841257c7c44b243214d4774e8310ecf2f02
Tikooh <JLRGeorge.Lo@gmail.com>
2023-12-14 10:00:08 +0000

    Tower Placement on grid works

--- Assets/scripts/towerPlacement.cs
+++ Assets/scripts/towerPlacement.cs
@@ -42,7 +42,7 @@ public class towerPlacement : MonoBehaviour, IDragHandler, IPointerDownHandler, 
                     }
                 }
 
-                gameObject.transform.position = Camera.main.ScreenToWorldPoint(pos);
+                gameObject.transform.position = pos;
             }
             else{
                 gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -57,40 +57,43 @@ public class towerPlacement : MonoBehaviour, IDragHandler, IPointerDownHandler, 
     {
         if (gameObject.tag == "towerPlaceable")
         {
-            Debug.Log("OnDrag");
+            // Debug.Log("OnDrag");
             relativeCameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             int x = (int) Math.Floor(relativeCameraPos.x);
             int y = (int) Math.Floor(relativeCameraPos.y);
-            Debug.Log(String.Format("x:{0}, y:{1}", x, y));
+
+            // Debug.Log(String.Format("x:{0}, y:{1}", x, y));
 
 
             if (x <= 4 && x >= -10)
             {
-                Debug.Log("here");
 
                 if ( y >= -3 && y <= 3)
                 {
 
                     Vector3 CompPos = new Vector3(x, y, 0);
+                    // Debug.Log(CompPos);
+                    print(grid.gridArray[0,0]);
                     
                     foreach (GridTile i in grid.gridArray)
                     {
-                        Debug.Log(String.Format("position:{0}, CompPos:{1}", i.position, CompPos));
                         if (i.position == CompPos)
                         {
-                            Debug.Log("here");
+                            // Debug.Log(String.Format("position:{0}, CompPos:{1}", i.position, CompPos));
+
                             i.towerPlaced = true;
-                            pos = i.position;
+                            pos = new Vector3((float) i.position.x, (float) i.position.y + 0.2f, 0);
+                            gameObject.transform.position = pos;
                         }
                     }
 
-                    gameObject.transform.position = Camera.main.ScreenToWorldPoint(pos);
+                    
                 }
 
             }
 
             else{
-                Debug.Log(String.Format("mouse:{0}, pos:{1}", relativeCameraPos, pos));
+                // Debug.Log(String.Format("mouse:{0}, pos:{1}", relativeCameraPos, pos));
 
                 gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector3 tmpPos = gameObject.transform.position; // Store all Vector3
